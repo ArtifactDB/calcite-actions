3da2ed8739a71a6cf61bd9cf1969d8fded4b3e69
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-23 16:03:52 -0700

    Switched to reading from issues to fire workflow.
    This is necessary as the REST API for workflow_dispatch doesn't return
    an ID, requiring us to do some further guesswork to actually get the
    exact link. Conversely, the issues API returns a number immediately.

--- src/index-project.js
+++ src/index-project.js
@@ -8,12 +8,13 @@
  * - R2_ACCOUNT_ID, the account id for Cloudflare's R2 storage.
  * - R2_ACCESS_KEY_ID, an authorized API key for Cloudflare R2 operations.
  * - R2_SECRET_ACCESS_KEY, an authorized API secret for Cloudflare R2 operations.
+ * - GITHUB_TOKEN, a token with read access to the GitHub CI repository reunning this script.
  *
  * In addition, we expect the following command-line variables:
  *
  * 1. The name of the R2 bucket of interest.
- * 2. The name of the project.
- * 3. (optional) The version of the project.
+ * 2. The full name (owner/repo) of the GitHub CI repository (i.e., the one containing this script).
+ * 3. The issue number.
  */
 
 import S3 from 'aws-sdk/clients/s3.js';
@@ -24,44 +25,87 @@ const s3 = new S3({
     secretAccessKey: `${process.env.R2_SECRET_ACCESS_KEY}`,
     signatureVersion: 'v4',
 });
+const token = process.env.GITHUB_TOKEN;
 
-if (process.argv.length < 4) {
-    throw new Error("expected 3 positional arguments - BUCKET_NAME, PROJECT_NAME and (optional) PROJECT_VERSION");
+if (process.argv.length - 2 != 3) {
+    throw new Error("expected 3 positional arguments - BUCKET_NAME, REPO_NAME and ISSUE_NUMBER");
 }
 const bucket_name = process.argv[2];
-const project = process.argv[3];
-const version = process.argv[4]; // TODO: make this optional
+const repo_name = process.argv[3];
+const issue_number = process.argv[4]; 
 
-// Listing all JSON files so we can pull them down in one big clump for each project.
-let params = { Bucket: bucket_name, Prefix: project + "/" + version };
+try {
+    // Fetching the issue data.
+    let body;
+    let title;
+    {
+        let res = await fetch("https://api.github.com/repos/" + repo_name + "/issues/" + issue_number, { headers: { Authorization: "Bearer " + token } });
+        if (!res.ok) {
+            throw new Error("failed to fetch issue parameters");
+        }
+        let info = await res.json();
+        body = JSON.parse(info.body);
+        title = info.title;
+    }
 
-let aggregated = [];
-while (1) {
-    let listing = s3.listObjectsV2(params)
-    let info = await listing.promise();
+    if (title == "upload complete") {
+        let project = body.project;
+        let version = body.version;
+        let permissions = body.permissions;
 
-    for (const f of info.Contents) {
-        if (!f.Key.endsWith(".json")) {
-            continue;
+        // Listing all JSON files so we can pull them down in one big clump for each project.
+        let params = { Bucket: bucket_name, Prefix: project + "/" + version };
+
+        let aggregated = [];
+        while (1) {
+            let listing = s3.listObjectsV2(params)
+            let info = await listing.promise();
+
+            for (const f of info.Contents) {
+                if (!f.Key.endsWith(".json")) {
+                    continue;
+                }
+
+                let res = s3.getObject({ Bucket: bucket_name, Key: f.Key });
+                let stuff = await res.promise();
+                aggregated.push(JSON.parse(stuff.Body.toString()));
+            }
+
+            if (info.IsTruncated) {
+                params.ContinuationToken = info.NextContinuationToken;
+            } else {
+                break;
+            }
         }
 
-        let res = s3.getObject({ Bucket: bucket_name, Key: f.Key });
-        let stuff = await res.promise();
-        aggregated.push(JSON.parse(stuff.Body.toString()));
+        // Saving the file.
+        let res = s3.putObject({ 
+            Bucket: bucket_name, 
+            Key: project + "/" + version + "/..aggregated.json", 
+            Body: JSON.stringify(aggregated), 
+            ContentType: "application/json" 
+        });
+        await res.promise();
     }
 
-    if (info.IsTruncated) {
-        params.ContinuationToken = info.NextContinuationToken;
-    } else {
-        break;
-    }
+    // Closing the issue once we're successfully done.
+    await fetch("https://api.github.com/repos/" + repo_name + "/issues/" + issue_number, { 
+        method: "PATCH",
+        body: JSON.stringify({ "state": "closed" }),
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+        } 
+    });
+
+} catch (e) {
+    // Commenting on the issue.
+    let res = await fetch("https://api.github.com/repos/" + repo_name + "/issues/" + issue_number + "/comments", { 
+        method: "POST",
+        body: JSON.stringify({ "body": e.message }),
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token
+        } 
+    });
 }
-
-// Saving the file.
-let res = s3.putObject({ 
-    Bucket: bucket_name, 
-    Key: project + "/" + version + "/..aggregated.json", 
-    Body: JSON.stringify(aggregated), 
-    ContentType: "application/json" 
-});
-await res.promise();
