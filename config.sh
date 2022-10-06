3673505d5882c066093a9a2a070de50397c6e3d8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-10-06 15:32:06 -0700

    Customized for calcite.

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 export CF_ACCOUNT_ID=bfb2e522e0b245720424784fcf7c04c0
-export SCHEMA_BUNDLE_URL=https://github.com/ArtifactDB/BiocObjectSchemas/releases/download/2022-10-01/bundle.tar.gz
-export R2_BUCKET_NAME=gypsum-test
+export SCHEMA_BUNDLE_URL=https://github.com/ArtifactDB/ExperimentHub-schemas/releases/download/2022-10-06/bundle.tar.gz
+export R2_BUCKET_NAME=calcite
